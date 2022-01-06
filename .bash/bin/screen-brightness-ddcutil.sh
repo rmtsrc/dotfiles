59c7bc8d64a7126caadbd714c75f9d1116135b90
rmt/src <144435+rmtsrc@users.noreply.github.com>
2022-01-06 16:57:34 +0000

    Fix optimus-manager blank screen on startup

--- .bash/bin/screen-brightness-ddcutil.sh
+++ .bash/bin/screen-brightness-ddcutil.sh
@@ -11,14 +11,15 @@
 # 1. Install `ddcutil`
 # 2. Load i2c: `sudo modprobe i2c-dev`
 # 3. Enable i2c at boot, add `i2c-dev` to: `/etc/modules-load.d/modules.conf`
-# 4. Allow non-root display access, add: `/etc/udev/rules.d/45-ddcutil-i2c.rules`
+# 4a i. Allow non-root display access, add: `/etc/udev/rules.d/45-ddcutil-i2c.rules`
 #      KERNEL=="i2c-[0-9]*", GROUP="your-user", MODE="0660", PROGRAM="/usr/bin/ddcutil --bus=%n getvcp 0x10"
-# 5. Reload rules: `sudo udevadm trigger`
-# 6. Find your monitor bus code: `ddcutil detect` # /dev/i2c-17 in my case
-# 7. Check what features can be controlled: `ddcutil capabilities --bus=17`
-# 8. Test get brightness: `ddcutil --bus=17 getvcp 10`
-# 9. Test set brightness 30%: `ddcutil --bus=17 setvcp 10 30`
-# 10. Update MONITOR= to match you monitor bus number
+# 4a ii. Reload rules: `sudo udevadm trigger`
+# 4b. Or allow user access to `i2c` group `sudo usermod -a -G i2c $USER`
+# 5. Find your monitor bus code: `ddcutil detect` # /dev/i2c-17 in my case
+# 6. Check what features can be controlled: `ddcutil capabilities --bus=17`
+# 7. Test get brightness: `ddcutil --bus=17 getvcp 10`
+# 8. Test set brightness 30%: `ddcutil --bus=17 setvcp 10 30`
+# 9. Update MONITOR= to match you monitor bus number
 
 # Stop on error
 set -e

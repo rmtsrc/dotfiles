ed7c586badc0eeed69e04fd2315a23e6235e6758
rmt/src <144435+rmtsrc@users.noreply.github.com>
2022-02-16 18:25:42 +0000

    Refactored hammerspoons into fns + screen PiP control + fixes

---
+++ .hammerspoon/mac-default.lua
@@ -0,0 +1,14 @@
+require "functions"
+
+hs.hotkey.bind({"ctrl"}, "home", splitWindowLeft)
+hs.hotkey.bind({"ctrl"}, "end", splitWindowRight)
+hs.hotkey.bind({"ctrl"}, "pageup", maximizeWindow)
+hs.hotkey.bind({"ctrl", "alt"}, "Right", growWindowWidth)
+hs.hotkey.bind({"ctrl", "alt"}, "Left", shrinkWindowWidth)
+hs.hotkey.bind({"ctrl", "alt"}, "Up", splitWindowTop)
+hs.hotkey.bind({"ctrl", "alt"}, "Down", splitWindowBottom)
+hs.hotkey.bind({"ctrl", "alt"}, "home", moveWindowToDisplay(1))
+hs.hotkey.bind({"ctrl", "alt"}, "end", moveWindowToDisplay(2))
+hs.hotkey.bind({"ctrl", "shift"}, "d", typeDate)
+hs.hotkey.bind({"ctrl", "cmd"}, "w", systemSleep)
+hs.hotkey.bind({"ctrl", "alt", "cmd"}, "forwarddelete", systemShutdown)

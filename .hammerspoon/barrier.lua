65b4dd2adadac7f68398d7e98f0981b51da063f8
rmt/src <144435+rmtsrc@users.noreply.github.com>
2022-04-01 17:28:15 +0100

    Config updates

---
+++ .hammerspoon/barrier.lua
@@ -0,0 +1,9 @@
+-- Linux logi mouse controls via barrier
+hs.hotkey.bind({"alt"}, "s", showHideMissionControl)
+hs.hotkey.bind({"ctrl", "cmd"}, "Left", switchToWorkspaceLeft)
+hs.hotkey.bind({"ctrl", "cmd"}, "Right", switchToWorkspaceRight)
+
+-- Windows Logitech Options via barrier
+hs.hotkey.bind({"alt"}, "tab", showHideMissionControl)
+hs.hotkey.bind({"alt"}, "Left", switchToWorkspaceLeft)
+hs.hotkey.bind({"alt"}, "Right", switchToWorkspaceRight)
--- .zshrc
+++ .zshrc
@@ -100,7 +100,7 @@ plugins=(
   git
   git-extras
   gitignore
-  gpg-agent
+  # gpg-agent
   history
   jsontools
   node

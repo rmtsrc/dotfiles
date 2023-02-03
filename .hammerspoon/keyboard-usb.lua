6bfac3758e3400f70b2c83b065d5cfb4dd875cef
rmt/src <144435+rmtsrc@users.noreply.github.com>
2023-02-03 10:41:32 +0000

    Updated Hammerspoon keyboard detection & tweaks

--- .hammerspoon/keyboard-usb.lua
+++ .hammerspoon/keyboard-usb.lua
@@ -1,6 +1,3 @@
-require "functions"
--- require "barrier"
-
 hs.hotkey.bind({"ctrl", "alt"}, "Left", splitWindowLeft)
 hs.hotkey.bind({"ctrl", "alt"}, "Right", splitWindowRight)
 hs.hotkey.bind({"ctrl", "alt"}, "Up", maximizeWindow)
@@ -10,6 +7,7 @@ hs.hotkey.bind({"alt", "cmd"}, "Up", splitWindowTop)
 hs.hotkey.bind({"alt", "cmd"}, "Down", splitWindowBottom)
 hs.hotkey.bind({"ctrl", "alt", "cmd"}, "Right", moveWindowToDisplay(1))
 hs.hotkey.bind({"ctrl", "alt", "cmd"}, "Left", moveWindowToDisplay(2))
+hs.hotkey.bind({"ctrl", "cmd"}, "d", toggleDarkMode)
 hs.hotkey.bind({"ctrl", "shift"}, "d", typeDate)
-hs.hotkey.bind({"ctrl", "cmd"}, "w", systemSleep)
+hs.hotkey.bind({"alt"}, "l", systemSleep)
 hs.hotkey.bind({"ctrl", "alt", "cmd"}, "forwarddelete", systemShutdown)

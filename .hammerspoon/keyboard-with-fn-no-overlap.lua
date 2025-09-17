3ca5226a362ace8dc0c41eee830cd0ed03da69b2
rmt/src <144435+rmtsrc@users.noreply.github.com>
2025-09-17 09:03:15 +0100

    Add new macOS show clipboard shortcut & `rm` to trash

--- .hammerspoon/keyboard-with-fn-no-overlap.lua
+++ .hammerspoon/keyboard-with-fn-no-overlap.lua
@@ -1,3 +1,4 @@
 hs.hotkey.bind({"ctrl"}, "home", windowMoveAndResize("Left"))
 hs.hotkey.bind({"ctrl"}, "end", windowMoveAndResize("Right"))
 hs.hotkey.bind({"ctrl"}, "pageup", windowFill)
+hs.hotkey.bind({"alt"}, "v", showClipboard)
